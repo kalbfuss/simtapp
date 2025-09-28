020d6724c58d3f8d8628f2402581e45b471a8aa7
Bernd Kalbfuss <langweiler@rueblitorte.de>
2025-09-28 08:40:42 +0200

    Added class MilestoneDate. Complete __repr__ methods of classes Milestone and MilestoneDate.

--- plog/models/milestone.py
+++ plog/models/milestone.py
@@ -36,6 +36,38 @@ class Milestone(Base):
         backref=backref("children", cascade="all, delete-orphan", passive_deletes=True)
     )
     project = relationship("Project", back_populates="milestones")
+    dates = relationship("MilestoneDate", back_populates="milestone", cascade="all, delete-orphan")
 
     def __repr__(self):
-        return f"<Milestone(title={self.title!r})>"
+        return (
+            f"<Milestone(id={self.id!r}, title={self.title!r}, description={self.description!r}, "
+            f"initial_baseline_date={self.initial_baseline_date!r}, latest_baseline_date={self.latest_baseline_date!r}, "
+            f"acceptance_criteria={self.acceptance_criteria!r}, project_id={self.project_id!r}, "
+            f"parent_id={self.parent_id!r}, created={self.created!r}, last_modified={self.last_modified!r})>"
+        )
+    
+    
+class MilestoneDate(Base):
+    """
+    Data model for the persistent storage of current milestone objects.
+    """
+    __versioned__ = {}
+    __tablename__ = "milestone_dates"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    milestone_id = Column(Integer, ForeignKey("milestones.id"), nullable=False)
+    date = Column(Date, nullable=True)
+    entry_date = Column(Date, nullable=True)
+    description = Column(Text, default="")
+    created = Column(DateTime, nullable=False)
+    last_modified = Column(DateTime, nullable=False)
+
+    milestone = relationship("Milestone", back_populates="dates")
+
+    def __repr__(self):
+        return (
+            f"<MilestoneDate(id={self.id!r}, milestone_id={self.milestone_id!r}, "
+            f"date={self.date!r}, entry_date={self.entry_date!r}, "
+            f"description={self.description!r}, created={self.created!r}, "
+            f"last_modified={self.last_modified!r})>"
+        )
